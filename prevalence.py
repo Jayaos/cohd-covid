b66b8b8751e007c0e0e1dd23ed761388dd255011
Jay Lee <jayaos1002@gmail.com>
2020-07-19 18:20:18 -0400

    update

--- prevalence.py
+++ prevalence.py
@@ -4,6 +4,12 @@ import os
 import datetime
 import codecs
 from collections import defaultdict
+from collections import namedtuple
+
+def strip_hypen(my_date):
+    splitted = my_date.split("-")
+    year = int(splitted[0])
+    return year, "".join(splitted)
 
 def logging_setup(output_dir):
     """ Set up for logging to log to file and to stdout
@@ -209,3 +215,228 @@ def load_patient_data(file, database, extra_header_lines_skip=0):
             
     fh.close()
     return patient_info
+
+def load_concept_patient_data(file, database, patient_info, extra_header_lines_skip=0, iatrogenic_ids=set()):
+    """Load concept-year-patient data
+    
+    Parameters
+    ----------
+    file: string - data file with concept_id, year, patient_id, and domain
+    database: string - Originating database. See _open_csv_reader
+    patient_info: object - Returned from load_patient_data
+    extra_header_lines_skip - int - Number of lines to skip after the header
+    
+    Returns
+    -------
+    ConceptPatientData object
+    """
+    logging.info("Loading condition, drug, and procedure data...")
+    
+    # Create an empty namedtuple for recording data
+    ConceptPatientData = namedtuple('ConceptPatientData', 
+    ['concept_year_patient', 'year_patient', 'year_numpatients'])
+
+    # Open csv reader
+    fh, reader = _open_csv_reader(file, database)
+
+    # Read header
+    header = next(reader)
+    columns = _find_columns(header, ['person_id', 'date', 'concept_id'])
+    table_width = len(header)
+
+    # Skip extra formatting lines after header
+    for i in range(extra_header_lines_skip):
+        next(reader)
+
+    # Read in each row of the file
+    concept_year_patient = defaultdict(lambda: defaultdict(set))
+    year_patients = defaultdict(set)
+    for row in reader:
+        # Display progress
+        if reader.line_num % 1000000 == 0:
+            logging.info(reader.line_num)
+            
+        if len(row) == table_width:
+            person_id, date_str, concept_id = [row[i] for i in columns]
+
+            # Convert concept_id to int
+            concept_id = int(concept_id)
+
+            # Skip when concept_id is 0 or iatrogenic
+            if concept_id == 0 or concept_id in iatrogenic_ids:
+                continue
+            
+            # Create new person_date_id
+            year, date_id = strip_hypen(date_str)
+            person_date_id = person_id + "_" + date_id
+
+            # Track concepts and patients by year
+            concept_year_patient[concept_id][year].add(person_date_id)
+            year_patients[year].add(person_date_id)
+
+    # For each patient seen in each year, add the patient's demographics (race, ethnicity, gender)
+    for year in year_patients:
+        patients_in_year = year_patients[year]
+        for person_date_id in patients_in_year:
+            person_id = person_date_id.split("_")[0]
+            pt_info = patient_info[person_id]
+            for concept_id in pt_info:
+                if concept_id != 0:
+                    concept_year_patient[concept_id][year].add(person_id)                     
+    
+    # Count how many patients in each year
+    year_numpatients = defaultdict(lambda: 0)
+    for year, pts in year_patients.items():
+        year_numpatients[year] = float(len(pts))
+    
+    logging.info("Loaded data for %d patients and %d concepts from %d rows." %
+                 (len(patient_info), len(concept_year_patient), reader.line_num))
+
+    fh.close()   
+    return ConceptPatientData(concept_year_patient, year_patients, year_numpatients)
+    
+def merge_concepts_years(cp_data, year_min, year_max):
+    """Merge data over the specified year range
+    
+    Parameters
+    ----------
+    cp_data: ConceptPatientData
+    year_min: int - First year in the range (inclusive)
+    year_max: int - Last year in the range (inclusive)
+    
+    Returns
+    -------
+    ConceptPatientDataMerged
+    """
+    # Create an empty namedtuple for recording data
+    ConceptPatientDataMerged = namedtuple('ConceptPatientDataMerged', 
+    ['concept_patient', 'patient', 'num_patients', 'year_min', 'year_max'])
+
+    logging.info('Merging concepts in range %d - %d' % (year_min, year_max))
+    
+    # How often to display progress message
+    concept_year_patient = cp_data.concept_year_patient
+    n_concepts = len(concept_year_patient)
+    progress_interval = round(n_concepts / 10)
+
+    # Collect all patients for each concept across the range of years
+    concepts_ranged = defaultdict(set)
+    for counter, concept_id in enumerate(concept_year_patient):
+        # Progress message
+        if counter % progress_interval == 0:
+            logging.info('%d%%' % round(counter / float(n_concepts) * 100))
+            
+        pts_merged = list()
+        for year, pts in concept_year_patient[concept_id].items():
+            # Skip if this is not in the desired year range
+            if year < year_min or year > year_max:
+                continue
+
+            # Combine list of patients and remove duplicates later (more efficient)
+            pts_merged.extend(pts)
+
+        if len(pts_merged) > 0:
+            concepts_ranged[concept_id] = set(pts_merged)
+    
+    # Merge the set of all patients across the years
+    year_patient = cp_data.year_patient
+    pts_merged = list()
+    for year, pts in year_patient.items():
+        if year >= year_min and year <= year_max:
+            # Note: faster to concatenate lists and then convert to set later
+            pts_merged.extend(year_patient[year])
+    pts_merged = set(pts_merged)
+    n_patients = float(len(pts_merged))
+
+    logging.info('%d concepts, %d patients (this is the denominator for prevalence)' %
+                 (len(concepts_ranged), n_patients))
+        
+    return ConceptPatientDataMerged(concepts_ranged, pts_merged, n_patients, year_min, year_max)
+
+def merge_ranged_concept_descendants(cp_ranged, concepts, descendants):
+    """Merge patients from descendant concepts.
+    Run this after merging patients by date.
+    Parameters
+    ----------
+    cp_ranged: ConceptPatientDataMerged
+    concepts: Dictionary of all observed concepts and their ancestors. The concepts included in this dictionary identify
+              which concepts to get merged patient sets for.
+    descendants: Dictionary of each concept's descendants (all descendants at all levels)
+    Returns
+    -------
+    ConceptPatientDataMerged
+    """
+    # Create a new namedtuple for recording data
+    ConceptPatientDataMerged = namedtuple('ConceptPatientDataMerged', 
+    ['concept_patient', 'patient', 'num_patients', 'year_min', 'year_max'])
+
+    logging.info('Merging concepts hierarchically for %d-%d dataset' % (cp_ranged.year_min, cp_ranged.year_max))
+
+    concept_patient = cp_ranged.concept_patient
+
+    # Keep track of which concepts are finished.
+    unfinished_concepts = set(concepts.keys())
+
+    # Loop until we have merged all hierarchical concepts
+    # Note: largest max_levels_of_separation in our OHDSI database is 24
+    max_iterations = 50
+    concepts_merged = defaultdict(set)
+    for i in range(max_iterations):
+        # Progress message
+        n_unfinished_concepts = len(unfinished_concepts)
+        logging.info('iteration %d: %d concepts remaining' % (i, n_unfinished_concepts))
+
+        # How often to display progress message
+        # progress_interval = round(n_unfinished_concepts / 10)	# Show progress every 10%
+        progress_interval = 0  # Don't show progress
+
+        # Keep track of which concepts were finished in this iteration
+        newly_finished_concepts = set()
+
+        # Merge patient sets for each concept if its descendants are finished
+        for j, concept_id in enumerate(unfinished_concepts):
+            # Progress message
+            if (progress_interval > 0) and (j % progress_interval == 0):
+                logging.info('%d%%' % round(j / float(n_unfinished_concepts) * 100))
+
+            # Check if the descendants are finished
+            descendants_finished = True
+            descendant_ids = descendants[concept_id]
+            for descendant_id in descendant_ids:
+                if descendant_id not in concepts_merged:
+                    descendants_finished = False
+                    break
+
+            if not descendants_finished:
+                # This concept's descendants are not finished yet. Skip.
+                continue
+
+            # This concept's descendants are finished. Merge the patients with its descendants
+            pts = list(concept_patient[concept_id])
+            for descendant_id in descendant_ids:
+                # Combine lists of patients now, remove duplicates later (more efficient)
+                pts.extend(concepts_merged[descendant_id])
+
+            # Save the set of unique patients and add this concept to the list of concepts finished in this iteration
+            concepts_merged[concept_id] = set(pts)
+            newly_finished_concepts.add(concept_id)
+
+        # Update the set of unfinished concepts
+        unfinished_concepts -= newly_finished_concepts
+
+        # Check if we're finished
+        if len(unfinished_concepts) == 0:
+            # No more concepts to do. Exit the loop.
+            break
+        elif len(newly_finished_concepts) == 0:
+            # Not done yet, but no new concepts were finished
+            logging.warning('merge_concept_descendants: No new concepts finished')
+        elif i == (max_iterations - 1):
+            # Reached the max iterations without finishing. Notify the user.
+            logging.warning('merge_concept_descendants: Terminated at max iterations without finishing')
+
+    logging.info('merge_concept_descendants: finished with %d concepts, %d patients' %
+                 (len(concepts_merged), len(cp_ranged.patient)))
+
+    return ConceptPatientDataMerged(concepts_merged, cp_ranged.patient, cp_ranged.num_patients,
+                                    cp_ranged.year_min, cp_ranged.year_max)   
